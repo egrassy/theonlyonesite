e338c617e5cad36d30d81427da21afbc5ed9d7a9
egrassy - TheOnlyOne <139762381+egrassy@users.noreply.github.com>
2026-08-09 08:03:21 +0200

    Simplify XXX DAO navigation script

--- scriptx.js
+++ scriptx.js
@@ -1,43 +1,15 @@
-$(document).ready(function () {
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+const menuButton = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.nav-links');
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        900,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
+menuButton?.addEventListener('click', () => {
+  const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
+  menuButton.setAttribute('aria-expanded', String(!isOpen));
+  menu.classList.toggle('is-open', !isOpen);
 });
 
-$(document).on("click", ".navbar-collapse.in", function (e) {
-  if ($(e.target).is("a:not('.dropdown-toggle')")) {
-    $(this).collapse("hide");
-  }
-});
-
-$(window).scroll(function () {
-  $(".slideanim").each(function () {
-    var pos = $(this).offset().top;
-
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      $(this).addClass("slide");
-    }
+menu?.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+    menuButton?.setAttribute('aria-expanded', 'false');
   });
 });
